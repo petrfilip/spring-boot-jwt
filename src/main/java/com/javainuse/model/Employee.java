7b292b6575348eb096dadbd4270ba7de1b3f8220
Petr Filip <petr.filip@plus4u.net>
2021-05-05 20:53:54 +0200

    Initial commit

---
+++ src/main/java/com/javainuse/model/Employee.java
@@ -0,0 +1,73 @@
+package com.javainuse.model;
+
+import java.util.Objects;
+
+public class Employee {
+	private Integer empId;
+	private String name;
+	private String designation;
+	private double salary;
+
+	public Employee() {
+	}
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Employee employee = (Employee) o;
+		return Double.compare(employee.salary, salary) == 0 && Objects.equals(empId, employee.empId) && Objects.equals(name, employee.name) && Objects
+				.equals(designation, employee.designation);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(empId, name, designation, salary);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"empId=" + empId +
+				", name='" + name + '\'' +
+				", designation='" + designation + '\'' +
+				", salary=" + salary +
+				'}';
+	}
+}
